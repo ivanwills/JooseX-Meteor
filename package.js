91a3d6ec4272c30f2fffb91d1ea9cddab18977c4
Ivan Wills <Ivan.Wills@optus.com.au>
2013-09-02 08:47:07 +1000

    Added EJSON as dependency

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+    api.use('ejson', ['client', 'server']);
     api.use('joose', ['client', 'server']);
 
     api.add_files('lib/JooseX/Meteor.js', ['server', 'client']);
